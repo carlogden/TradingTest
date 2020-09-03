12c6f4842f4753c1d12fc4fadef253d93f9b1bdf
Carl Ogden <carlo@ogdens.org>
2020-09-02 17:29:44 -0700

    Add Etrade API Keys

---
+++ TradingTest/APIKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingTest
+{
+    public class APIKey
+    {
+        public string API { get; set; }
+        public string Secret { get; set; }
+
+    }
+}
---
+++ TradingTest/DataSource/Etrade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradingTest;
+
+namespace TradingTest.DataSource
+{
+    public class Etrade
+    {
+        private APIKey Production = new APIKey { API = "78b054c67ac6a3483cf5a7c116c120f4", Secret = "565338627f877af609c995bedb6b0646a70f61a9fbd85587eeb24f535e134220" };
+        private APIKey Sandbox = new APIKey { API = "0871814057b9dcc4483933d1527d3a21", Secret = "f199a949fac1a7d612ba953405d29bc671c2f56c9ad0c10db3f36467f5b88456" };
+
+
+    }
+}
